4580f4cf6b2381c78e766478315b63fa04ce1c8a
unknown <moisesjosemiranda@gmail.com>
2019-04-22 16:34:07 -0300

    .net frame to 4.5

--- src/CreateUserFields/Program.cs
+++ src/CreateUserFields/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,10 +19,24 @@ namespace CreateUserFields
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            var sqlConn = new SQLiteConnection(@"Data Source=Data\CreateUserFields.db;Version=3;");
-            var conn = new DataConnection(sqlConn);
-            DataConnection.SetConnection(conn);
-            Application.Run(new FormCreateUserFields());
+            try
+            {
+                var sqlConn = new SQLiteConnection(@"Data Source=Data\CreateUserFields.db;Version=3;");
+                var conn = new DataConnection(sqlConn);
+                DataConnection.SetConnection(conn);
+                Application.Run(new FormCreateUserFields());
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show($"DI API não instalada erro: {ex.Message}");
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw ex;
+            }
+            
         }
     }
 }
